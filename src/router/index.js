d3ba45aa7e1e7ef2185469a0a23b8f84bc89997c
Sam McCall <smccall@smccall.com>
2020-10-29 10:13:01 -0400

    Reogranize routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,26 @@
 import Vue from "vue";
 import Router from "vue-router";
+import VueMeta from "vue-meta";
 
 import Home from "./../components/pages/Home.vue";
 import Documentation from "./../components/pages/Documentation.vue";
 
 Vue.use(Router);
+Vue.use(VueMeta);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  {
+    path: "/documentation",
+    name: "Documentation",
+    component: Documentation
+  }
+];
 
 export default new Router({
-  routes: [
-    { path: "/", name: "Home", component: Home },
-    { path: "/documentation", name: "Documentation", component: Documentation }
-  ]
+  routes
 });
